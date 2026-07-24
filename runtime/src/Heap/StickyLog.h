5069abdd5deb65ba686bc1381e77a913404a9521
Zxilly <zxilly@outlook.com>
2026-07-25 01:49:17 +0800

    feat(gc): export sticky heap size

--- runtime/src/Heap/StickyLog.h
+++ runtime/src/Heap/StickyLog.h
@@ -16,6 +16,7 @@
 namespace MapleRuntime {
 extern "C" MRT_EXPORT uint8_t* __cj_sticky_logged_base;
 extern "C" MRT_EXPORT uintptr_t __cj_sticky_heap_base;
+extern "C" MRT_EXPORT uintptr_t __cj_sticky_heap_size;
 extern "C" MRT_EXPORT const uint8_t __cj_sticky_line_shift;
 extern "C" MRT_EXPORT void CJ_MCC_StickyLogLine(BaseObject* object);
 
