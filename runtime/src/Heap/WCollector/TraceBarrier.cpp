e4b619f4bf080bbdfbef766c4f6782bfb7c1406f
Zxilly <zxilly@outlook.com>
2026-08-01 02:01:51 +0800

    semantic(gc): type trace barrier object consumption
    Anchor: runtime/src/Heap/WCollector/TraceBarrier.cpp:74-352

--- runtime/src/Heap/WCollector/TraceBarrier.cpp
+++ runtime/src/Heap/WCollector/TraceBarrier.cpp
@@ -71,7 +71,7 @@ void TraceBarrier::ReadStruct(MAddress dst, BaseObject* obj, MAddress src, size_
 {
     LocalRefFieldContainer refFields;
     if (obj != nullptr) {
-        obj->ForEachRefInStruct(
+        ForEachRefInStruct(UnsafeAssumeCurrent(obj),
             [&refFields, dst, src, size](RefField<false>& field) {
                 if (reinterpret_cast<MAddress>(&field) < src || reinterpret_cast<MAddress>(&field) >= (src + size)) {
                     return;
@@ -153,17 +153,18 @@ void TraceBarrier::WriteStaticRef(RefField<false>& field, BaseObject* ref) const
 void TraceBarrier::WriteStruct(BaseObject* obj, MAddress dst, size_t dstLen, MAddress src, size_t srcLen) const
 {
     CHECK(obj != nullptr);
+    CurrentPtr currentObject = UnsafeAssumeCurrent(obj);
     if (obj != nullptr) {
         MRT_ASSERT(dst > reinterpret_cast<MAddress>(obj), "WriteStruct struct addr is less than obj!");
         Mutator* mutator = Mutator::GetMutator();
         if (mutator != nullptr) {
-            obj->ForEachRefInStruct(
+            ForEachRefInStruct(currentObject,
                 [=](RefField<>& refField) {
                     mutator->RememberObjectInSatbBuffer(ReadReference(obj, refField));
                 },
                 dst, dst + dstLen);
         }
-        obj->ForEachRefInStruct(
+        ForEachRefInStruct(currentObject,
             [=](RefField<>& refField) {
                 MAddress offset = reinterpret_cast<MAddress>(&refField) - dst;
                 RefField<> srcField(*reinterpret_cast<RefField<>*>(src + offset));
@@ -175,7 +176,7 @@ void TraceBarrier::WriteStruct(BaseObject* obj, MAddress dst, size_t dstLen, MAd
     CHECK(memcpy_s(reinterpret_cast<void*>(dst), dstLen, reinterpret_cast<void*>(src), srcLen) == EOK);
 
     if (obj != nullptr) {
-        obj->ForEachRefInStruct(
+        ForEachRefInStruct(currentObject,
             [=](RefField<>& refField) {
                 RefField<> oldField(refField);
                 MAddress oldValue = oldField.GetFieldValue();
@@ -252,8 +253,9 @@ void TraceBarrier::AtomicWriteReference(BaseObject* obj, RefField<true>& field, 
     field.SetFieldValue(newField.GetFieldValue(), order);
     mutator->RememberObjectInSatbBuffer(oldRef);
     if (obj != nullptr) {
-        DLOG(TBARRIER, "atomic write obj %p<%p>(%zu) ref@%p: %#zx -> %#zx", obj, obj->GetTypeInfo(), obj->GetSize(),
-             &field, oldValue, newField.GetFieldValue());
+        CurrentPtr currentObject = UnsafeAssumeCurrent(obj);
+        DLOG(TBARRIER, "atomic write obj %p<%p>(%zu) ref@%p: %#zx -> %#zx", obj, GetTypeInfo(currentObject),
+             GetSize(currentObject), &field, oldValue, newField.GetFieldValue());
     } else {
         DLOG(TBARRIER, "atomic write static ref@%p: %#zx -> %#zx", &field, oldValue, newField.GetFieldValue());
     }
@@ -270,8 +272,9 @@ BaseObject* TraceBarrier::AtomicSwapReference(BaseObject* obj, RefField<true>& f
     RefField<> oldField(oldValue);
     BaseObject* oldRef = ReadReference(nullptr, oldField);
     mutator->RememberObjectInSatbBuffer(oldRef);
-    DLOG(TRACE, "atomic swap obj %p<%p>(%zu) ref-field@%p: old %#zx(%p), new %#zx(%p)", obj, obj->GetTypeInfo(),
-         obj->GetSize(), &field, oldValue, oldRef, field.GetFieldValue(), newRef);
+    CurrentPtr currentObject = UnsafeAssumeCurrent(obj);
+    DLOG(TRACE, "atomic swap obj %p<%p>(%zu) ref-field@%p: old %#zx(%p), new %#zx(%p)", obj,
+         GetTypeInfo(currentObject), GetSize(currentObject), &field, oldValue, oldRef, field.GetFieldValue(), newRef);
     FixEdgeSet::Instance().MaybeAdd(obj, reinterpret_cast<RefField<>*>(&field), newRef);
     return oldRef;
 }
@@ -303,12 +306,13 @@ bool TraceBarrier::CompareAndSwapReference(BaseObject* obj, RefField<true>& fiel
 void TraceBarrier::CopyStructArray(BaseObject* dstObj, MAddress dstField, MIndex dstSize, BaseObject* srcObj,
                                    MAddress srcField, MIndex srcSize) const
 {
+    CurrentPtr currentDestination = UnsafeAssumeCurrent(dstObj);
 #if defined(MRT_DEBUG) && (MRT_DEBUG == 1)
-    if (!dstObj->HasRefField()) {
+    if (!HasRefField(currentDestination)) {
         LOG(RTLOG_FATAL, "array %p doesn't have class-type element\n", dstObj);
         return;
     }
-    if (!(static_cast<MArray*>(dstObj)->GetComponentTypeInfo()->IsStructType())) {
+    if (!GetComponentTypeInfo(currentDestination)->IsStructType()) {
         LOG(RTLOG_FATAL, "array %p type is not struct type", dstObj);
         return;
     }
@@ -325,7 +329,7 @@ void TraceBarrier::CopyStructArray(BaseObject* dstObj, MAddress dstField, MIndex
         }
     };
     MArray* srcArray = static_cast<MArray*>(srcObj);
-    srcArray->ForEachRefFieldInRange(srcVisitor, srcField, srcField + srcSize);
+    ForEachRefFieldInRange(UnsafeAssumeCurrent(srcArray), srcVisitor, srcField, srcField + srcSize);
 
     RefFieldVisitor dstVisitor = [this, mutator](RefField<false>& field) {
         RefField<> oldField(field);
@@ -333,7 +337,7 @@ void TraceBarrier::CopyStructArray(BaseObject* dstObj, MAddress dstField, MIndex
         mutator->RememberObjectInSatbBuffer(target);
     };
     MArray* dstArray = static_cast<MArray*>(dstObj);
-    dstArray->ForEachRefFieldInRange(dstVisitor, dstField, dstField + srcSize);
+    ForEachRefFieldInRange(currentDestination, dstVisitor, dstField, dstField + srcSize);
 
     CHECK_DETAIL(memmove_s(reinterpret_cast<void*>(dstField), dstSize, reinterpret_cast<void*>(srcField), srcSize) ==
                      EOK,
@@ -347,7 +351,8 @@ void TraceBarrier::CopyStructArray(BaseObject* dstObj, MAddress dstField, MIndex
 
 void TraceBarrier::WriteGeneric(const ObjectPtr obj, void* fieldPtr, const ObjectPtr src, size_t size) const
 {
-    if ((obj != nullptr && !obj->HasRefField()) || (!Heap::IsHeapAddress(obj) && !Heap::IsHeapAddress(src))) {
+    if ((obj != nullptr && !HasRefField(UnsafeAssumeCurrent(obj))) ||
+        (!Heap::IsHeapAddress(obj) && !Heap::IsHeapAddress(src))) {
         CHECK_DETAIL(memcpy_s(fieldPtr, size,
                               reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(src) + TYPEINFO_PTR_SIZE),
                               size) == EOK,
