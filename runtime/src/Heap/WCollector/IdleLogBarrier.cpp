619b114f8c54c34d81a779d1f672305b40bd0e28
Zxilly <zxilly@outlook.com>
2026-07-31 20:29:21 +0800

    test(gc): retain real stores for STW edge control

--- runtime/src/Heap/WCollector/IdleLogBarrier.cpp
+++ runtime/src/Heap/WCollector/IdleLogBarrier.cpp
@@ -13,10 +13,8 @@ ALWAYS_INLINE void IdleLogBarrier::LogObject(BaseObject* obj) const { CJ_MCC_Sti
 
 void IdleLogBarrier::WriteReference(BaseObject* obj, RefField<false>& field, BaseObject* ref) const
 {
-    if (!StickyLog::Instance().ShouldDropEdgeCompleteStore(
-            obj, reinterpret_cast<MAddress>(&field), ref)) {
-        LogObject(obj);
-    }
+    StickyLog::Instance().RecordEdgeCompleteStoreCandidate(obj, reinterpret_cast<MAddress>(&field), ref);
+    LogObject(obj);
     IdleBarrier::WriteReference(obj, field, ref);
 }
 
@@ -34,20 +32,16 @@ void IdleLogBarrier::WriteStruct(BaseObject* obj, MAddress dst, size_t dstLen, M
 void IdleLogBarrier::AtomicWriteReference(BaseObject* obj, RefField<true>& field, BaseObject* ref,
                                           MemoryOrder order) const
 {
-    if (!StickyLog::Instance().ShouldDropEdgeCompleteStore(
-            obj, reinterpret_cast<MAddress>(&field), ref)) {
-        LogObject(obj);
-    }
+    StickyLog::Instance().RecordEdgeCompleteStoreCandidate(obj, reinterpret_cast<MAddress>(&field), ref);
+    LogObject(obj);
     IdleBarrier::AtomicWriteReference(obj, field, ref, order);
 }
 
 BaseObject* IdleLogBarrier::AtomicSwapReference(BaseObject* obj, RefField<true>& field, BaseObject* ref,
                                                 MemoryOrder order) const
 {
-    if (!StickyLog::Instance().ShouldDropEdgeCompleteStore(
-            obj, reinterpret_cast<MAddress>(&field), ref)) {
-        LogObject(obj);
-    }
+    StickyLog::Instance().RecordEdgeCompleteStoreCandidate(obj, reinterpret_cast<MAddress>(&field), ref);
+    LogObject(obj);
     return IdleBarrier::AtomicSwapReference(obj, field, ref, order);
 }
 
