1b611d70975b67d2e4aebe3357f8f8162d82100e
Zxilly <zxilly@outlook.com>
2026-08-01 01:59:25 +0800

    semantic(gc): preserve const field owner address
    Anchor: runtime/src/ObjectModel/Field.inline.h:22

--- runtime/src/ObjectModel/Field.inline.h
+++ runtime/src/ObjectModel/Field.inline.h
@@ -19,7 +19,7 @@ template<typename T, bool isAtomic>
 void Field<T, isAtomic>::SetFieldValue(const BaseObject* obj, T v, std::memory_order order)
 {
     DLOG(BARRIER, "write obj %p(%zu)+%zu field @%p 0x%zx -> 0x%zx", obj,
-         GetSize(UnsafeAssumeCurrent(obj)),
+         GetSize(UnsafeAssumeCurrent(const_cast<BaseObject*>(obj))),
          BaseObject::FieldOffset(obj, this), this, value, v);
 
     if (isAtomic) {
