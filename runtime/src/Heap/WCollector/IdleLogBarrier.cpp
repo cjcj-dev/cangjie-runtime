2f5725726c2f60f0980e2dacfc2e3c90013f7f35
Zxilly <zxilly@outlook.com>
2026-08-01 03:02:17 +0800

    measure(gc): hook idle barrier reference writes (IdleLogBarrier.cpp:15)

--- runtime/src/Heap/WCollector/IdleLogBarrier.cpp
+++ runtime/src/Heap/WCollector/IdleLogBarrier.cpp
@@ -6,15 +6,21 @@
 
 #include "IdleLogBarrier.h"
 
+#include "Heap/RemsetCheck.h"
 #include "Heap/StickyLog.h"
+#include "ObjectModel/MArray.h"
 
 namespace MapleRuntime {
 ALWAYS_INLINE void IdleLogBarrier::LogObject(BaseObject* obj) const { CJ_MCC_StickyLogLine(obj); }
 
 void IdleLogBarrier::WriteReference(BaseObject* obj, RefField<false>& field, BaseObject* ref) const
 {
+    RemsetCheck& check = RemsetCheck::Instance();
+    check.RecordHookHit(RemsetCheck::HookSite::WRITE_REFERENCE);
     LogObject(obj);
     IdleBarrier::WriteReference(obj, field, ref);
+    check.RecordBarrierEdge(obj, reinterpret_cast<MAddress>(&field), ref,
+                            RemsetCheck::HookSite::WRITE_REFERENCE);
 }
 
 void IdleLogBarrier::WriteStaticRef(RefField<false>& field, BaseObject* ref) const
@@ -24,40 +30,78 @@ void IdleLogBarrier::WriteStaticRef(RefField<false>& field, BaseObject* ref) con
 
 void IdleLogBarrier::WriteStruct(BaseObject* obj, MAddress dst, size_t dstLen, MAddress src, size_t srcLen) const
 {
+    RemsetCheck& check = RemsetCheck::Instance();
+    check.RecordHookHit(RemsetCheck::HookSite::WRITE_STRUCT);
     LogObject(obj);
     IdleBarrier::WriteStruct(obj, dst, dstLen, src, srcLen);
+    if (check.IsEnabled() && obj != nullptr && Heap::IsHeapAddress(obj)) {
+        obj->ForEachRefInStruct(
+            [&check, obj](RefField<false>& field) {
+                check.RecordBarrierEdge(obj, reinterpret_cast<MAddress>(&field), field.GetTargetObject(),
+                                        RemsetCheck::HookSite::WRITE_STRUCT);
+            },
+            dst, dst + dstLen);
+    }
 }
 
 void IdleLogBarrier::AtomicWriteReference(BaseObject* obj, RefField<true>& field, BaseObject* ref,
                                           MemoryOrder order) const
 {
+    RemsetCheck& check = RemsetCheck::Instance();
+    check.RecordHookHit(RemsetCheck::HookSite::ATOMIC_WRITE_REFERENCE);
     LogObject(obj);
     IdleBarrier::AtomicWriteReference(obj, field, ref, order);
+    check.RecordBarrierEdge(obj, reinterpret_cast<MAddress>(&field), ref,
+                            RemsetCheck::HookSite::ATOMIC_WRITE_REFERENCE);
 }
 
 BaseObject* IdleLogBarrier::AtomicSwapReference(BaseObject* obj, RefField<true>& field, BaseObject* ref,
                                                 MemoryOrder order) const
 {
+    RemsetCheck& check = RemsetCheck::Instance();
+    check.RecordHookHit(RemsetCheck::HookSite::ATOMIC_SWAP_REFERENCE);
     LogObject(obj);
-    return IdleBarrier::AtomicSwapReference(obj, field, ref, order);
+    BaseObject* oldRef = IdleBarrier::AtomicSwapReference(obj, field, ref, order);
+    check.RecordBarrierEdge(obj, reinterpret_cast<MAddress>(&field), ref,
+                            RemsetCheck::HookSite::ATOMIC_SWAP_REFERENCE);
+    return oldRef;
 }
 
 bool IdleLogBarrier::CompareAndSwapReference(BaseObject* obj, RefField<true>& field, BaseObject* oldRef,
                                              BaseObject* newRef, MemoryOrder succOrder, MemoryOrder failOrder) const
 {
+    RemsetCheck& check = RemsetCheck::Instance();
+    check.RecordHookHit(RemsetCheck::HookSite::COMPARE_AND_SWAP_REFERENCE);
     LogObject(obj);
-    return IdleBarrier::CompareAndSwapReference(obj, field, oldRef, newRef, succOrder, failOrder);
+    bool exchanged = IdleBarrier::CompareAndSwapReference(obj, field, oldRef, newRef, succOrder, failOrder);
+    if (exchanged) {
+        check.RecordBarrierEdge(obj, reinterpret_cast<MAddress>(&field), newRef,
+                                RemsetCheck::HookSite::COMPARE_AND_SWAP_REFERENCE);
+    }
+    return exchanged;
 }
 
 void IdleLogBarrier::CopyStructArray(BaseObject* dstObj, MAddress dstField, MIndex dstSize, BaseObject* srcObj,
                                      MAddress srcField, MIndex srcSize) const
 {
+    RemsetCheck& check = RemsetCheck::Instance();
+    check.RecordHookHit(RemsetCheck::HookSite::COPY_STRUCT_ARRAY);
     LogObject(dstObj);
     IdleBarrier::CopyStructArray(dstObj, dstField, dstSize, srcObj, srcField, srcSize);
+    if (check.IsEnabled() && dstObj != nullptr && Heap::IsHeapAddress(dstObj)) {
+        MArray* dstArray = static_cast<MArray*>(dstObj);
+        dstArray->ForEachRefFieldInRange(
+            [&check, dstObj](RefField<false>& field) {
+                check.RecordBarrierEdge(dstObj, reinterpret_cast<MAddress>(&field), field.GetTargetObject(),
+                                        RemsetCheck::HookSite::COPY_STRUCT_ARRAY);
+            },
+            dstField, dstField + dstSize);
+    }
 }
 
 void IdleLogBarrier::WriteGeneric(const ObjectPtr obj, void* fieldPtr, const ObjectPtr src, size_t size) const
 {
+    RemsetCheck::Instance().RecordHookHit(RemsetCheck::HookSite::WRITE_GENERIC);
     IdleBarrier::WriteGeneric(obj, fieldPtr, src, size);
 }
 } // namespace MapleRuntime
