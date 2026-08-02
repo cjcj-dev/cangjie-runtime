e56a342b21a0650441975c2f1cb9cee3101c6716
Zxilly <zxilly@outlook.com>
2026-08-03 02:08:38 +0800

    semantic: make reference-writing barrier entrypoints non-virtual

--- runtime/src/Heap/Barrier/Barrier.h
+++ runtime/src/Heap/Barrier/Barrier.h
@@ -9,6 +9,7 @@
 #define MRT_BARRIER_H
 
 #include "Common/BaseObject.h"
+#include "Heap/StickyLog.h"
 #include "ObjectModel/Field.h"
 #include "ObjectModel/MClass.h"
 
@@ -34,33 +35,87 @@ public:
     virtual void ReadStruct(MAddress dst, BaseObject* obj, MAddress src, size_t size) const;
     virtual void ReadStaticStruct(MAddress dst, MAddress src, size_t size, const GCTib gctib) const;
 
-    virtual void WriteReference(BaseObject* obj, RefField<false>& field, BaseObject* ref) const;
+    void WriteReference(BaseObject* obj, RefField<false>& field, BaseObject* ref) const
+    {
+        RecordCrossGenerationEdge(obj);
+        WriteReferenceImpl(obj, field, ref);
+    }
     virtual void WriteStaticRef(RefField<false>& field, BaseObject* ref) const;
-    virtual void WriteStruct(BaseObject* obj, MAddress dst, size_t dstLen, MAddress src, size_t srcLen) const;
+    void WriteStruct(BaseObject* obj, MAddress dst, size_t dstLen, MAddress src, size_t srcLen) const
+    {
+        RecordCrossGenerationEdge(obj);
+        WriteStructImpl(obj, dst, dstLen, src, srcLen);
+    }
     virtual void WriteStaticStruct(MAddress dst, size_t dstLen, MAddress src, size_t srcLen, const GCTib gctib) const;
 
-    virtual void CopyRefArray(BaseObject* dstObj, MAddress dstField, MIndex dstSize,
-                              BaseObject* srcObj, MAddress srcField, MIndex srcSize) const;
-    virtual void CopyStructArray(BaseObject* dstObj, MAddress dstField, MIndex dstSize,
-                              BaseObject* srcObj, MAddress srcField, MIndex srcSize) const;
+    void CopyRefArray(BaseObject* dstObj, MAddress dstField, MIndex dstSize,
+                      BaseObject* srcObj, MAddress srcField, MIndex srcSize) const
+    {
+        RecordCrossGenerationEdge(dstObj);
+        CopyRefArrayImpl(dstObj, dstField, dstSize, srcObj, srcField, srcSize);
+    }
+    void CopyStructArray(BaseObject* dstObj, MAddress dstField, MIndex dstSize,
+                         BaseObject* srcObj, MAddress srcField, MIndex srcSize) const
+    {
+        RecordCrossGenerationEdge(dstObj);
+        CopyStructArrayImpl(dstObj, dstField, dstSize, srcObj, srcField, srcSize);
+    }
 
     virtual BaseObject* AtomicReadReference(BaseObject* obj, RefField<true>& field,
                                             MemoryOrder order) const;
 
-    virtual void AtomicWriteReference(BaseObject* obj, RefField<true>& field, BaseObject* ref, MemoryOrder order) const;
-    virtual BaseObject* AtomicSwapReference(BaseObject* obj, RefField<true>& field, BaseObject* ref,
-                                            MemoryOrder order) const;
-    virtual bool CompareAndSwapReference(BaseObject* obj, RefField<true>& field, BaseObject* oldRef, BaseObject* newRef,
-                                         MemoryOrder succOrder, MemoryOrder failOrder) const;
+    void AtomicWriteReference(BaseObject* obj, RefField<true>& field, BaseObject* ref, MemoryOrder order) const
+    {
+        RecordCrossGenerationEdge(obj);
+        AtomicWriteReferenceImpl(obj, field, ref, order);
+    }
+    BaseObject* AtomicSwapReference(BaseObject* obj, RefField<true>& field, BaseObject* ref,
+                                    MemoryOrder order) const
+    {
+        RecordCrossGenerationEdge(obj);
+        return AtomicSwapReferenceImpl(obj, field, ref, order);
+    }
+    bool CompareAndSwapReference(BaseObject* obj, RefField<true>& field, BaseObject* oldRef, BaseObject* newRef,
+                                 MemoryOrder succOrder, MemoryOrder failOrder) const
+    {
+        RecordCrossGenerationEdge(obj);
+        return CompareAndSwapReferenceImpl(obj, field, oldRef, newRef, succOrder, failOrder);
+    }
 
     // helper for delegation
     template<typename T>
     inline void WriteField(BaseObject* obj, Field<T>& field, T val) const;
 
-    virtual void WriteGeneric(const ObjectPtr obj, void* fieldPtr, const ObjectPtr src, size_t size) const;
-    virtual void ReadGeneric(const ObjectPtr dstPtr, ObjectPtr obj, void* fieldPtr, size_t size) const;
+    void WriteGeneric(const ObjectPtr obj, void* fieldPtr, const ObjectPtr src, size_t size) const
+    {
+        if (obj != nullptr && obj->HasRefField()) {
+            RecordCrossGenerationEdge(obj);
+        }
+        WriteGenericImpl(obj, fieldPtr, src, size);
+    }
+    void ReadGeneric(const ObjectPtr dstPtr, ObjectPtr obj, void* fieldPtr, size_t size) const
+    {
+        RecordCrossGenerationEdge(dstPtr);
+        ReadGenericImpl(dstPtr, obj, fieldPtr, size);
+    }
 
 protected:
+    virtual void WriteReferenceImpl(BaseObject* obj, RefField<false>& field, BaseObject* ref) const;
+    virtual void WriteStructImpl(BaseObject* obj, MAddress dst, size_t dstLen, MAddress src, size_t srcLen) const;
+    virtual void CopyRefArrayImpl(BaseObject* dstObj, MAddress dstField, MIndex dstSize,
+                                  BaseObject* srcObj, MAddress srcField, MIndex srcSize) const;
+    virtual void CopyStructArrayImpl(BaseObject* dstObj, MAddress dstField, MIndex dstSize,
+                                     BaseObject* srcObj, MAddress srcField, MIndex srcSize) const;
+    virtual void AtomicWriteReferenceImpl(BaseObject* obj, RefField<true>& field, BaseObject* ref,
+                                          MemoryOrder order) const;
+    virtual BaseObject* AtomicSwapReferenceImpl(BaseObject* obj, RefField<true>& field, BaseObject* ref,
+                                                MemoryOrder order) const;
+    virtual bool CompareAndSwapReferenceImpl(BaseObject* obj, RefField<true>& field, BaseObject* oldRef,
+                                             BaseObject* newRef, MemoryOrder succOrder,
+                                             MemoryOrder failOrder) const;
+    virtual void WriteGenericImpl(const ObjectPtr obj, void* fieldPtr, const ObjectPtr src, size_t size) const;
+    virtual void ReadGenericImpl(const ObjectPtr dstPtr, ObjectPtr obj, void* fieldPtr, size_t size) const;
+
     class LocalRefFieldContainer {
     public:
         // multi-thread unsafe.
@@ -90,6 +145,9 @@ protected:
         std::vector<RefField<>*> excessive;
     };
     Collector& theCollector;
+
+private:
+    ALWAYS_INLINE void RecordCrossGenerationEdge(BaseObject* obj) const { CJ_MCC_StickyLogLine(obj); }
 };
 } // namespace MapleRuntime
 #endif // ~MRT_BARRIER_H
