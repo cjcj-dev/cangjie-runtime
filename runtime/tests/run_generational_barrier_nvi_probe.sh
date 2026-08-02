8a386c41cb45857cca4c8d89e04f57f268d8cb49
Zxilly <zxilly@outlook.com>
2026-08-03 03:16:01 +0800

    fix(gc): include bounds checking headers in NVI probe (run_generational_barrier_nvi_probe.sh:27)

--- runtime/tests/run_generational_barrier_nvi_probe.sh
+++ runtime/tests/run_generational_barrier_nvi_probe.sh
@@ -21,7 +21,8 @@ cleanup()
 trap cleanup EXIT
 
 taskset -c "$cpuset" clang++ -std=gnu++14 -O2 -pthread \
-    -I"$repo/runtime/src" "$repo/runtime/tests/generational_barrier_nvi_harness.cpp" \
+    -I"$repo/runtime/src" -I"$repo/runtime/third_party/third_party_bounds_checking_function/include" \
+    "$repo/runtime/tests/generational_barrier_nvi_harness.cpp" \
     -L"$runtime_lib_dir" -Wl,-rpath,"$runtime_lib_dir" -lcangjie-runtime -lboundscheck \
     -o "$probe_tmp/gcv2-nvi-probe"
 LD_LIBRARY_PATH="$runtime_lib_dir${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}" \
