cbc51cff3f8c98dc323cc6c9f882760203ab117b
Zxilly <zxilly@outlook.com>
2026-08-03 13:26:01 +0800

    instrument(gc): label minor root fix snapshots

--- runtime/src/Heap/WCollector/WCollector.h
+++ runtime/src/Heap/WCollector/WCollector.h
@@ -206,7 +206,7 @@ private:
     void RescanRememberedSet(WorkStack& workStack, const MinorSlotSet& rememberedSlots,
                              const MinorSlotSet& reachableSlots, const MinorSlotSet& weakSlots, bool fullYoungScan);
     bool FixMinorEvacuatedSlot(RefField<>& field) const;
-    void FixMinorRootSlots();
+    void FixMinorRootSlots(const char* moment);
     void FixMinorObjectSlots(BaseObject* object);
     void EvacuateYoungRegions(const MinorObjectSet& reachableObjects, const MinorSlotSet& rememberedSlots);
     void ValidateYoungMarking(const MinorObjectSet& reachableObjects, const MinorObjectSet& allocationRoots);
