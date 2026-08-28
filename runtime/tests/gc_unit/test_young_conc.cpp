9ee96100ab3432cc9d5f7b95a3b1fa9a20f6f7ae
Zxilly <zxilly@outlook.com>
2026-08-28 20:49:37 +0800

    fix(gc): keep refproc DiscoverWeak import outside TESTABLE wrap (run_standalone.sh:304)
    mc 把 test_young_conc 整 TU 门控后，默认构型 ELF 不再导入 DiscoverWeakReference。

--- runtime/tests/gc_unit/test_young_conc.cpp
+++ runtime/tests/gc_unit/test_young_conc.cpp
@@ -1,5 +1,3 @@
-#if defined(MRT_TESTABLE_INTERNALS)
-
 // Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
 // This source file is part of the Cangjie project, licensed under Apache-2.0
 // with Runtime Library Exception.
@@ -75,6 +73,8 @@ GC_TEST(ReferenceProcessor, WeakDiscoveryPublishesNoStrongMarkWork)
     processor.ProcessReferences([](BaseObject*) { return true; });
     processor.EnqueueReferences([](BaseObject*) {});
 }
+
+#if defined(MRT_TESTABLE_INTERNALS)
 
 extern "C" int CJ_ScheduleManagerInit();
 extern "C" bool MRT_NewForeignCJThread();
