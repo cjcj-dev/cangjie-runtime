4248b49ffd2c05320af105423e16f452df5dd931
Zxilly <zxilly@outlook.com>
2026-09-13 01:21:15 +0800

    fix(gc): add MarkTerminate resurrected (zMarkTerminate.inline.hpp:125-138)

--- runtime/src/Heap/Collector/MarkEngine.h
+++ runtime/src/Heap/Collector/MarkEngine.h
@@ -36,6 +36,8 @@ public:
     bool Saturated() const;
     bool Terminated() const;
     size_t WorkerCount() const;
+    void SetResurrected(bool value) { resurrected.store(value, std::memory_order_relaxed); }
+    bool Resurrected() const { return resurrected.load(std::memory_order_relaxed); }
 
 private:
     void MaybeReduceStripes(MarkStripeSet& stripes, size_t usedNStripes);
@@ -45,6 +47,7 @@ private:
     size_t working = 0;
     size_t awakening = 0;
     bool terminated = false;
+    std::atomic<bool> resurrected{ false };
     mutable std::mutex mutex;
     std::condition_variable condition;
 };
