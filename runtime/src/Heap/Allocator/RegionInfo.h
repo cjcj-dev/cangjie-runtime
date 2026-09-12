e6bb1fb085db668debc3338185e3d5339422dcae
Zxilly <zxilly@outlook.com>
2026-09-12 18:09:25 +0800

    fix(gc): RouteInfo slot pad is 24B not 32B (zPage.cpp:90)
    UnitInfo static_assert 248: former RouteInfo was 24 bytes.

--- runtime/src/Heap/Allocator/RegionInfo.h
+++ runtime/src/Heap/Allocator/RegionInfo.h
@@ -3944,7 +3944,7 @@ private:
         // at ClearLiveInfo / mark-start, are implicitly live (zPage.inline.hpp:180-185
         // is_allocating). 0 = no mark-start yet.
         uintptr_t markStartAllocPtr;
-        uint8_t routeInfoPad[32] = {};
+        uint8_t routeInfoPad[24] = {};
         uint64_t snapshotEpoch = 0;
         // used to traverse ghost region.
         uint32_t nextRegionIdx0;
