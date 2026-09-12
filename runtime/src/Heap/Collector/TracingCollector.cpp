b5699fc9216657852ac1e0eb89a8b0e1eecbb432
Zxilly <zxilly@outlook.com>
2026-09-12 15:31:04 +0800

    fix(gc): process weak roots before relocation selection (zGeneration.cpp:1330)

--- runtime/src/Heap/Collector/TracingCollector.cpp
+++ runtime/src/Heap/Collector/TracingCollector.cpp
@@ -4,6 +4,7 @@
 //
 // See https://cangjie-lang.cn/pages/LICENSE for license information.
 
+#include "Mutator/WeakRefBuffer.h"
 #include "Heap/Collector/MarkEngine.h"
 #include "Heap/Collector/MarkStripe.h"
 #include "TracingCollector.h"
@@ -914,6 +915,9 @@ void TracingCollector::ProcessOldNonStrongReferences(WorkStack& workStack)
         MRT_PHASE_TIMER("concurrent resurrection");
         DoResurrection(workStack);
     }
+    // ZGenerationOld::process_non_strong_references: weak roots follow the
+    // finalizable closure, before relocation-set processing.
+    WeakRefBuffer::Instance().ClearWeakRefBuffer();
 }
 
 bool TracingCollector::FinishOldMark(WorkStack& workStack)
