dc5cab011d3a4066c972d4a7e6fd0ed84eefbc7d
Zxilly <zxilly@outlook.com>
2026-09-17 23:11:00 +0800

    feat(gc): young concurrent_mark/pause_mark_end hit ZBreakpoint (zGeneration.cpp:1088)
    ZGC old concurrent_mark calls at_after_marking_started / at_before_marking_completed.
    Young collect is the WB_BREAKPOINT prelude; fire the same named points so
    ConcurrentGCBreakpoints.RunTo observes them on the product collect path.

--- runtime/src/Heap/z/zGeneration.cpp
+++ runtime/src/Heap/z/zGeneration.cpp
@@ -527,6 +527,7 @@ void WCollector::RunYoungCollection()
 
 void WCollector::ConcurrentYoungMark()
 {
+    ZBreakpoint::AtAfterMarkingStarted();
     uint64_t stackScanEpoch = youngStackScanEpoch;
     WorkStack& workStack = youngWorkStack;
     constexpr bool fullYoungScan = false;
@@ -658,6 +659,7 @@ void WCollector::ConcurrentYoungMark()
 
 bool WCollector::YoungMarkEndPause()
 {
+    ZBreakpoint::AtBeforeMarkingCompleted();
     WorkStack& workStack = youngWorkStack;
 #if defined(MRT_TESTABLE_INTERNALS)
     const uint64_t markEndPauseStartNs = TimeUtil::NanoSeconds();
