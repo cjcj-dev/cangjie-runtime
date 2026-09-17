93c28d1a8042b2e2abb01c51618884bbb482920e
Zxilly <zxilly@outlook.com>
2026-09-18 07:02:51 +0800

    Revert "test(gc): MarkPublicationFixture drains Heap unique collector (zCollectedHeap.cpp:62)"
    This reverts commit f90e07a49562bfcc7c0e589d470cd11539f35ab2.

--- runtime/tests/gc_unit/mark_publication_fixture.hpp
+++ runtime/tests/gc_unit/mark_publication_fixture.hpp
@@ -13,12 +13,18 @@ struct MarkPublicationFixture {
     MarkPublicationFixture* previousFixture = current;
     static MarkPublicationFixture& Current() { CHECK(current != nullptr); return *current; }
     CollectorResources& resources = Heap::GetHeap().GetCollectorResources();
-    WCollector& collector = static_cast<WCollector&>(Heap::GetHeap().GetCollector());
+    WCollector collector { Heap::GetHeap().GetAllocator(), resources };
+    Collector* previousCollector;
     MarkPublicationFixture()
+        : previousCollector(resources.testCollector)
     {
         current = this;
         collector.youngCycle.InitializeWorkers(1);
         collector.oldCycle.InitializeWorkers(1);
+        if (previousCollector != nullptr) {
+            GcUnit::GcHeapFixture::AdoptGenerationIdentity(collector, *previousCollector);
+        }
+        resources.testCollector = &collector;
         collector.youngCycle.SelectReason(GC_REASON_YOUNG);
         collector.youngCycle.Begin(1);
         // ZGenerationYoung::mark_start advances the sequence with the remset
@@ -35,6 +41,7 @@ struct MarkPublicationFixture {
     ~MarkPublicationFixture()
     {
         Drain([](BaseObject*, bool) {});
+        resources.testCollector = previousCollector;
         current = previousFixture;
     }
     template<class Visitor> void DrainDomain(ZMark& domain, Visitor&& visitor)
