258ed9fd44b3bfa57c8f4fd27b5361161fc71fd4
Zxilly <zxilly@outlook.com>
2026-09-17 15:43:21 +0800

    fix(gc): revert managed-root notes that compiled into product

--- runtime/src/Heap/z/zStackWatermark.cpp
+++ runtime/src/Heap/z/zStackWatermark.cpp
@@ -7,7 +7,6 @@
 #include "Heap/z/zThreadLocalAllocBuffer.hpp"
 #include "Heap/z/zThreadLocalData.hpp"
 #include "Heap/z/zUncoloredRoot.inline.hpp"
-#include "ObjectModel/MArray.h"
 #include "Mutator/Mutator.h"
 #include "UnwindStack/StackFrameCursor.h"
 
@@ -82,10 +81,6 @@ void StackWatermark::process_head(Mutator& mutator, void* context, const RootVis
         const uintptr_t color = mutator.GetGCData().loadGoodMask != 0 ? mutator.GetGCData().loadGoodMask
                                                                      : ZPointerLoadGoodMask;
         ZUncoloredRoot::process_invisible(invisible, color);
-#if defined(MRT_GC_UNIT_TESTS)
-        NoteLargeArrayInitRootVisit(LargeArrayRootVisitSite::STACK_WATERMARK_MANAGED,
-                                    mutator.LoadInvisibleRoot());
-#endif
     }
 }
 
