765fded5db00b46992dd902af46b230171b8e816
Zxilly <zxilly@outlook.com>
2026-09-18 19:21:47 +0800

    refactor(gc): drop unused resource stats proxies (zGeneration.hpp:65-86)

--- runtime/src/Heap/z/zDriver.hpp
+++ runtime/src/Heap/z/zDriver.hpp
@@ -397,10 +397,6 @@ public:
     ~CollectorResources() = default;
 
 
-    ZWorkers& GetWorkers(ZGenerationId generation) const;
-
-    GCStats& GetGCStats(ZGenerationId generation = ZGenerationId::old);
-
 #if defined(MRT_TESTABLE_INTERNALS)
     friend struct RelocationReceiptTestAccess;
     friend struct MarkPort203TestAccess;
