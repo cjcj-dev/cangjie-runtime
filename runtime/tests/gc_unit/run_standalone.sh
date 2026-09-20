3461426415dcb34784504cd5e2ade5512bb008f6
Zxilly <zxilly@outlook.com>
2026-09-20 11:28:47 +0800

    test(gc): do not compile P1 mark-start tests that need deleted testMarkStartState

--- runtime/tests/gc_unit/run_standalone.sh
+++ runtime/tests/gc_unit/run_standalone.sh
@@ -365,7 +365,6 @@ MAIN_SOURCES=(
   "$SRC/test_partial_array.cpp"
   "$SRC/test_segmented_array_init.cpp"
   "$SRC/test_allocation_stall_queue.cpp"
-  "$SRC/test_p1_mark_start.cpp"
   "$SRC/test_package_init.cpp"
   "$SRC/test_verify_roots.cpp"
   "$SRC/test_p10_roots_iterator.cpp"
