c45311bb9805de32a5fd915db6cef65fb65754ca
Zxilly <zxilly@outlook.com>
2026-09-23 01:04:05 +0800

    fix(gc): read director busy gates at decision points (ZGC zDirector.cpp:608)

--- runtime/src/Heap/z/zDirector.hpp
+++ runtime/src/Heap/z/zDirector.hpp
@@ -15,8 +15,6 @@ private:
     std::condition_variable condition;
     bool stopped = false;
     bool reevaluate = false;
-    bool minorBusy = false;
-    bool majorBusy = false;
 
     bool wait_for_tick();
 
@@ -29,8 +27,6 @@ public:
 
     static void evaluate_rules();
     void notify_reevaluate();
-    void set_busy(bool minor, bool busy);
-    bool busy(bool minor);
 };
 } // namespace MapleRuntime
 
